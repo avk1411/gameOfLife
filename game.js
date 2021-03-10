110699b940299c4f958efc77765b2e64434845ec
Apurva Kulkarni <apurva.k.kulkarni@oracle.com>
2021-03-10 22:40:52 +0530

    UI updates

--- game.js
+++ game.js
@@ -1,36 +1,40 @@
 var grid;
 var ctx;
 var shape = [];
-var span; 
+var xSpan; 
+var ySpan;
 var size;
 var loop;
 var pattern;
+var isRunning = false;
 $(function () {
     grid = document.getElementById("grid");
     ctx = grid.getContext("2d");
     grid.addEventListener("click", clickCell, false);
     // https://stackoverflow.com/questions/6348494/addeventlistener-vs-onclick
-    size = 20;
-    drawGrid(size);
+    xSize = 20;
+    ySize = 15;
+    drawGrid(xSize, ySize);
     shape = [[1, 0], [2, 1], [2, 2], [1, 2], [0, 2]];
     console.log(shape);
     //drawShape(shape);
     console.log("jQuery function complete");
 });
 
-function drawGrid(size) {
+function drawGrid(xSize, ySize) {
     ctx.save();
-    span = grid.width / size;
+    xSpan = grid.width / xSize;
+    ySpan = grid.height / ySize;
     //create one grid elem
     var canvasPattern = document.createElement("canvas");
-    canvasPattern.width = span;
-    canvasPattern.height = span;
+    canvasPattern.width = xSpan;
+    canvasPattern.height = ySpan;
     var contextPattern = canvasPattern.getContext("2d");
     contextPattern.lineWidth = 1.0;
     contextPattern.strokeStyle= 'rgba(0,0,0,1.0)';
     contextPattern.fillStyle = 'rgba(255,255,255,1.0)';
-    contextPattern.fillRect(0,0,span,span);
-    contextPattern.strokeRect(0, 0, span, span);
+    contextPattern.fillRect(0,0,xSpan,ySpan);
+    contextPattern.strokeRect(0, 0, xSpan, ySpan);
 
     pattern = ctx.createPattern(canvasPattern, "repeat");
     ctx.fillStyle = pattern;
@@ -50,9 +54,9 @@ function drawShape(shape) {
     ctx.globalCompositeOperation = "xor";
     
     shape.forEach(function (cell, i) {
-        ctx.clearRect(cell[0]*span, cell[1]*span, span, span);
-        ctx.fillRect(cell[0]*span, cell[1]*span, span, span);
-        ctx.strokeRect(cell[0]*span, cell[1]*span, span, span);
+        ctx.clearRect(cell[0]*xSpan, cell[1]*ySpan, xSpan, ySpan);
+        ctx.fillRect(cell[0]*xSpan, cell[1]*ySpan, xSpan, ySpan);
+        ctx.strokeRect(cell[0]*xSpan, cell[1]*ySpan, xSpan, ySpan);
     });
   
 }
@@ -60,35 +64,35 @@ function clickCell(event) {
     ctx.strokeStyle = 'rgba(0,0,0,1.0)'; //todo
     ctx.lineWidth = 1.0;
     
-    var xVal = (event.pageX - grid.offsetLeft) / span;
+    var xVal = (event.pageX - grid.offsetLeft) / xSpan;
     xVal = Math.floor(xVal);
-    var yVal = (event.pageY - grid.offsetTop) / span;
+    var yVal = (event.pageY - grid.offsetTop) / ySpan;
     yVal = Math.floor(yVal);
     console.log(xVal, yVal);
 
 
-    var imgData = ctx.getImageData((xVal * span) + (span/2) , (yVal * span) + (span/2), span, span).data;
+    var imgData = ctx.getImageData((xVal * xSpan) + (xSpan/2) , (yVal * ySpan) + (ySpan/2), xSpan, ySpan).data;
     console.log(imgData);
 
     //Deslected Cell should get selected
     if(imgData[2]==0 || imgData[0]==255){
         console.log("SELECTING");
         console.log(imgData[3]);
-        ctx.clearRect(xVal * span, yVal * span, span, span);
-        ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
-        ctx.fillRect(xVal * span, yVal * span, span, span);
+        ctx.clearRect(xVal * xSpan, yVal * ySpan, xSpan, ySpan);
+        ctx.fillStyle = 'rgba(37,5,130,1)';
+        ctx.fillRect(xVal * xSpan, yVal * ySpan, xSpan, ySpan);
         ctx.lineWidth=1.0;
-        ctx.strokeRect(xVal * span, yVal * span, span, span);
+        ctx.strokeRect(xVal * xSpan, yVal * ySpan, xSpan, ySpan);
         shape.push([xVal,yVal]);
     }
     //selected cell needs to be deselected
     else{
         console.log("DESELECTED");
         //ctx.fillStyle = 'rgba(0,0,0,0)';
-        ctx.clearRect(xVal * span, yVal * span, span, span);
+        ctx.clearRect(xVal * xSpan, yVal * ySpan, xSpan, ySpan);
         //ctx.fillRect(xVal * span, yVal * span, span, span);
         ctx.lineWidth=1.0;
-        ctx.strokeRect(xVal * span, yVal * span, span, span);
+        ctx.strokeRect(xVal * xSpan, yVal * ySpan, xSpan, ySpan);
         shape.splice(shape.indexOf([xVal,yVal]),1);
     }
 }
@@ -129,19 +133,19 @@ function nextGen() {
     deselect.forEach(function (cell, i) {
 
         //ctx.fillStyle = 'rgba(0,0,0,0)';
-        ctx.clearRect(cell[0] * span, cell[1] * span, span, span);
+        ctx.clearRect(cell[0] * xSpan, cell[1] * ySpan, xSpan, ySpan);
        // ctx.fillRect(cell[0] * span, cell[1] * span, span, span);
         ctx.lineWidth = 1.0;
-        ctx.strokeRect(cell[0] * span, cell[1] * span, span, span);
+        ctx.strokeRect(cell[0] * xSpan, cell[1] * ySpan, xSpan, ySpan);
         shape.splice(shape.indexOf(cell), 1);        
     });
     select.forEach(function (cell,i) {
 
-        ctx.clearRect(cell[0] * span, cell[1] * span, span, span);
-        ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
-        ctx.fillRect(cell[0] * span, cell[1] * span, span, span);
+        ctx.clearRect(cell[0] * xSpan, cell[1] * ySpan, xSpan, ySpan);
+        ctx.fillStyle = 'rgba(37,5,130,1)';
+        ctx.fillRect(cell[0] * xSpan, cell[1] * ySpan, xSpan, ySpan);
         ctx.lineWidth = 1.0;
-        ctx.strokeRect(cell[0] * span, cell[1] * span, span, span);
+        ctx.strokeRect(cell[0] * xSpan, cell[1] * ySpan, xSpan, ySpan);
         shape.push(cell);
     });
 
@@ -160,8 +164,8 @@ function countNeighbours(cell) {
             }
             var xVal = cell[0] +i;
             var yVal = cell[1] +j;
-            if(xVal > -1 && yVal> -1 && xVal <size && yVal<size){
-                var imgData = ctx.getImageData((xVal * span) + (span / 2), (yVal * span) + (span / 2), span, span).data;
+            if(xVal > -1 && yVal> -1 && xVal <xSize && yVal<ySize){
+                var imgData = ctx.getImageData((xVal * xSpan) + (xSpan / 2), (yVal * ySpan) + (ySpan / 2), xSpan, ySpan).data;
                 if (imgData[2] != 0 && imgData[0] != 255) {
                     count++;
                 }
@@ -176,6 +180,10 @@ function countNeighbours(cell) {
 }
 
 function  startSim() {
+    if(isRunning){
+        return;
+    }
+    isRunning= true;
     var count = document.getElementById("genCount").innerHTML;
     loop =setInterval(function name(params) {
         nextGen();
@@ -186,6 +194,7 @@ function  startSim() {
 }
 function stopSim() {
     clearInterval(loop);
+    isRunning= false;
 }
 function clearGrid() {
 
